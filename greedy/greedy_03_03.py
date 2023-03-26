a0fcf6759a0a271e7adc3177b6b78ed504922eb5
Humpty <35593604+jungphilip@users.noreply.github.com>
2023-03-26 09:52:46 +0900

    Create greedy_03_03.py

---
+++ greedy/greedy_03_03.py
@@ -0,0 +1,12 @@
+// 그리디 03_03 숫자카드게임
+// min 사용해보기
+
+n, m = map(int, input().split())
+result = 0
+
+for i in range(n):
+  arr = list(map(int, input().split()))
+  if result < min(arr):
+    result = min(arr)
+
+print(result)
