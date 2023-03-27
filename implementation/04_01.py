4387d90af69ae33770ca43c327ab3b13ee7abf56
Humpty <35593604+jungphilip@users.noreply.github.com>
2023-03-28 00:17:56 +0900

    simplified ver (feat. jy)

--- implementation/04_01.py
+++ implementation/04_01.py
@@ -6,24 +6,16 @@ y = 1
 
 for i in range(len(arr)):
   if arr[i] == 'L':
-    if y == 1 :
-      pass
-    else:
+    if y != 1 :
       y -= 1
   if arr[i] == 'R':
-    if y == 5 :
-      pass
-    else:
+    if y != n :
       y += 1
   if arr[i] == 'U':
-    if x == 1 :
-      pass
-    else:
+    if x != 1 :
       x -= 1
   if arr[i] == 'D':
-    if x == 5 :
-      pass
-    else:
+    if x != n :
       x += 1
 
 print(x, y)
